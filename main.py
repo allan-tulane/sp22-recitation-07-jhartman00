9d0a3f6bf2ca576bda553c960dec099ebbc7c5f1
jhartman00 <79610215+jhartman00@users.noreply.github.com>
2022-04-14 15:43:57 +0000

    did a lot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math, queue
 from collections import Counter
+from bitarray import bitarray
 
 class TreeNode(object):
     # we assume data is a tuple (frequency, character)
@@ -7,10 +8,12 @@ class TreeNode(object):
         self.left = left
         self.right = right
         self.data = data
+        self.code = ''
     def __lt__(self, other):
         return(self.data < other.data)
     def children(self):
         return((self.left, self.right))
+    
     
 def get_frequencies(fname):
     ## This function is done.
@@ -25,8 +28,7 @@ def get_frequencies(fname):
 # create a prefix code tree using Huffman's algorithm
 def make_huffman_tree(f):
     p = queue.PriorityQueue()
-    # construct heap from frequencies, the initial items should be
-    # the leaves of the final tree
+    # construct heap from frequencies, the initial items should be the leaves of the final tree
     for c in f.keys():
         p.put(TreeNode(None,None,(f[c], c)))
 
@@ -34,28 +36,69 @@ def make_huffman_tree(f):
     # create a new node z with x and y as children,
     # insert z into the priority queue (using an empty character "")
     while (p.qsize() > 1):
-        # TODO
-        
+        x = p.get()
+        x.code = 0
+        y = p.get()
+        y.code = 1
+        z = TreeNode(x, y,(x.data[0]+y.data[0], ""))
+        p.put(z)
+
     # return root of the tree
     return p.get()
 
 # perform a traversal on the prefix code tree to collect all encodings
 def get_code(node, prefix="", code={}):
     # TODO - perform a tree traversal and collect encodings for leaves in code
-    pass
+    #Inorder Traversal Recursive
+
+    prefix = prefix + str(node.code)
+    
+    if node.left:
+        get_code(node.left, prefix)
+    if node.right:
+        get_code(node.right, prefix)
+
+    if(not node.left and not node.right):
+        code[node.data[1]] = prefix
+
+    return code
 
 # given an alphabet and frequencies, compute the cost of a fixed length encoding
 def fixed_length_cost(f):
-    # TODO
-    pass
+    dict = get_frequencies(f)
+    n = len(dict.keys())
+    cost = math.log(n)
+    return cost
+    
 
 # given a Huffman encoding and character frequencies, compute cost of a Huffman encoding
 def huffman_cost(C, f):
     # TODO
     pass
 
-f = get_frequencies('f1.txt')
-print("Fixed-length cost:  %d" % fixed_length_cost(f))
-T = make_huffman_tree(f)
-C = get_code(T)
-print("Huffman cost:  %d" % huffman_cost(C, f))
+# f = get_frequencies('f1.txt')
+# print("Fixed-length cost:  %d" % fixed_length_cost(f))
+# T = make_huffman_tree(f)
+# C = get_code(T)
+# print("Huffman cost:  %d" % huffman_cost(C, f))
+
+def encode(data, coding):
+    encoded = []
+    for element in data:
+        print(coding[element], end = "")
+        encoded.append(coding[element])
+    out = ''.join([str(item) for item in encoded])
+    return out
+
+
+if __name__ == "__main__":
+    # print(fixed_length_cost("alice29.txt"))
+    f = get_frequencies("f1.txt")
+    h = make_huffman_tree(f)
+    code = get_code(h)
+    print(code)
+    data = list(open("f1.txt").read())
+    encoded = encode(data, code)
+    print("\n\n\n\n")
+    decoded = bitarray(encode).decode(code)
+    print("".join(decoded))
